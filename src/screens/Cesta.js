b86733edc9f9ca73710a28e8958bcf995d811293
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-04-18 21:40:36 -0300

    feat: adding basket details

--- src/screens/Cesta.js
+++ src/screens/Cesta.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Image, StyleSheet, Dimensions, Text } from 'react-native'
+import { Image, StyleSheet, Dimensions, Text, View } from 'react-native'
 
 import topo from '../../assets/topo.png'
+import logo from '../../assets/logo.png'
 
 const width = Dimensions.get('screen').width
 
@@ -13,6 +14,18 @@ export default function Cesta(){
                 style={styles.topo}
             />
             <Text style={styles.titulo}>Detalhes da cesta</Text>
+
+            <View style={styles.cestaView}>
+                <Text style={styles.nome}>Cesta de verduras</Text>
+                <View style={styles.fazendaView}>
+                    <Image source={logo} style={styles.imagemFazenda}/>
+                    <Text style={styles.nomeFazenda}>Jenny Jack Farm</Text>
+                </View>
+                <Text style={styles.descricao}>
+                    Uma cesta com produtos selecionados cuidadosamente da fazendo direto para a sua cozinha
+                </Text>
+                <Text style={styles.preco}>R$ 40,00</Text>
+            </View>
         </>
     )
 }
@@ -31,5 +44,40 @@ const styles = StyleSheet.create({
         color: 'white',
         fontWeight: 'bold',
         padding: 16
+    },
+    cestaView: {
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+    nome: {
+        color: '#464646',
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: 'bold'
+    },
+    fazendaView: {
+        flexDirection: 'row',
+        paddingVertical: 12
+    },
+    imagemFazenda: {
+        width: 32,
+        height: 32
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12
+    },
+    descricao: {
+        color: '#A3A3A3',
+        fontSize: 16,
+        lineHeight: 26
+    },
+    preco: {
+        color: '#2A9F85',
+        fontWeight: 'bold',
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
     }
 })
