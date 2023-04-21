3b4cf08b0b79e112397300efef5ee67bf4635d93
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-04-21 11:07:39 -0300

    feat: creating the item component

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -1,5 +1,11 @@
 import logo from '../../assets/logo.png';
 
+import abobora from '../../assets/frutas/Abóbora.png'
+import batata from '../../assets/frutas/Batata.png'
+import brocolis from '../../assets/frutas/Brócolis.png'
+import pepino from '../../assets/frutas/Pepino.png'
+import tomate from '../../assets/frutas/Tomate.png'
+
 const cesta = {
     topo: {
         titulo: 'Detalhes da cesta'
@@ -11,6 +17,31 @@ const cesta = {
         descricao: 'Uma cesta com produtos selecionados cuidadosamente da fazendo direto para a sua cozinha',
         preco: 'R$ 40,00',
         botao: 'Comprar',
+    },
+    itens: {
+        titulo: 'Itens da cesta',
+        lista: [
+            {
+                nome: 'Abóbora',
+                imagem: abobora,
+            },
+            {
+                nome: 'Batata',
+                imagem: batata,
+            },
+            {
+                nome: 'Brócolis',
+                imagem: brocolis
+            },
+            {
+                nome: 'Pepino',
+                imagem: pepino,
+            },
+            {
+                nome: 'Tomate',
+                imagem: tomate,
+            },
+        ]
     }
 }
 
